26715791762eb054b9c91b4b3918fd222f796d0e
Igor Nikitin <rabbitinspace@icloud.com>
2020-07-01 23:33:33 +1000

    fix relative url redirects

--- Gemini/Sources/Gemini/Payload.swift
+++ Gemini/Sources/Gemini/Payload.swift
@@ -5,26 +5,19 @@ public struct GeminiRequest {
     public private(set) var url: URL
 
     public init(url: URL) {
-        guard var components = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
-            preconditionFailure("not a url: \(url)")
-        }
-
-//        if components.scheme == nil {
-//            components.scheme = "gemini"
-//        }
-//
-//        if components.port == nil {
-//            components.port = 1965
-//        }
-//
-//        precondition(components.scheme == "gemini", "only gemini scheme is supported")
-//        precondition(components.host?.isEmpty == false, "host is required")
-        self.url = components.url!
+        self.url = url
     }
 
     func withURL(_ url: URL) -> GeminiRequest {
         var request = self
-        request.url = url
+        var newURL = url
+
+        // that should probably mean that url is relative
+        if newURL.scheme == nil {
+            newURL = URL(string: url.relativeString, relativeTo: request.url) ?? url
+        }
+
+        request.url = newURL
         return request
     }
 
